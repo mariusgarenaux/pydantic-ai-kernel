c8cce791e1a0a04f8530c054645c216a0999a1c2
Marius Garénaux <marius.garenaux-gruau@irisa.fr>
2025-12-19 15:39:53 +0100

    bump 1.2.0

--- pydantic_ai_kernel/__init__.py
+++ pydantic_ai_kernel/__init__.py
@@ -1,7 +1,7 @@
 """An example Jupyter kernel"""
 
-__version__ = "1.1.1"
+__version__ = "1.2.0"
 
 
-from .kernel import PydanticAIBaseKernel
-from .agent_config import AgentConfig, ModelProviderConfig, ModelConfig
+from .kernel import PydanticAIBaseKernel  # noqa: F401
+from .agent_config import AgentConfig, ModelProviderConfig, ModelConfig  # noqa: F401
